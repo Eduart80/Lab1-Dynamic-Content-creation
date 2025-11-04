1457469a6137055f581a0350d05db08d6f1a4dfd
Eduart2021 <zeedymarina@yahoo.com>
2025-11-04 14:12:32 -0600

    adding calcutation function

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ const btnAdd = document.getElementById("batch-add")
 const productList = document.getElementById('product-List')
 const HTMLCountList = document.getElementsByClassName('items')
 const cartCounter = document.getElementById('cart-counter')
+const price = document.getElementById('price')
+const totalPrice = document.getElementById('priceTotal')
+
+const cartItems = [
+    { name: "Blue Parrot", price: 120, quantity: 1 },
+    { name: "Multicolor Parrot", price: 250, quantity: 1 },
+    { name: "Red Parrot", price: 850, quantity: 1 },
+    { name: "Small Parrot", price: 99, quantity: 1 },
+    { name: "White Parrot", price: 500, quantity: 1 },
+];
 
 // Shopping cart counter
 let cartCount = 0
+let totalPriceCalc = 0
 
 // Function to update cart counter
 function updateCartCounter() {
@@ -42,8 +53,9 @@ productList.addEventListener('click',(event)=>{
         // Increment cart counter
         cartCount++
         updateCartCounter()
-        
+        updateTotalDisplay()
     }
+
 })
 //remove
 UiList.addEventListener('click', function(event) {
@@ -63,3 +75,11 @@ UiList.addEventListener('click', function(event) {
         }
     }
 });
+
+function calculatorTotal(items){
+    const total = items.reduce((accumulator, currentItem)=>{
+        const itemCost = currentItem.price * currentItem.quantity
+        return accumulator + itemCost
+    },0)
+    return total
+}
